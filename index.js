fb000e105365f1b659108612c186fb57299256a3
TiDu <tidu.nguyen.2000@gmail.com>
2020-10-21 20:14:52 +0700

    fixed port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.env.PORT || 6969
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Listening on ${port}`)
 })
