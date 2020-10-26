afbdd037c5189553c5f09d6141b52ccbcaca30ad
TiDu <tidu.nguyen.2000@gmail.com>
2020-10-26 22:01:07 +0700

    added express session and md5 hash for storing password

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -2,10 +2,10 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const { Pool, Client } = require('pg')
 const pool = new Pool({
-	user: 'xlsdzjfqchkfwl',
-	host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
-	database: 'd3bk8eqdhtlqr4',
-	password: '5cfc3aba9bb6887f7e5bfd167aec789549507b081dae7220295a8737fa6ae8e8',
+	user: 'ckbdoamkelurck',
+	host: 'ec2-54-157-4-216.compute-1.amazonaws.com',
+	database: 'deqcemi6em18vn',
+	password: 'ac1a5efd48319b30480b625170bb5ca5504e7e92cbd44b9429ac7df90fb0be7a',
 	port: 5432,
 	ssl: true
 })
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,22 @@
+const { Hash } = require('crypto');
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const path = require('path');
-const pg = require('pg');
 const db = require('./db');
+const md5 = require('crypto').createHash('md5');
 const port = process.env.PORT || 3000;
 
+// functions
+do_hash = (s) => md5.update(s).digest('hex');
+
 app.use(express.json());
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: 'ShigeoTokuda',
+	cookie: { maxAge: 31536000000 }
+}));
 app.use(express.static(path.join(__dirname, '../../root')));
 
 // api
@@ -15,7 +26,7 @@ app.post("/login", loginAccount);
 // register account function
 async function registerAccount(req, res) {
 	try {
-		const result = await db.query(`INSERT INTO accounts (name, email, password, phone) VALUES($1,$2,$3,$4)`, [req.body.name, req.body.email, req.body.password, req.body.phone]);
+		const result = await db.query(`INSERT INTO accounts (name, email, password, phone) VALUES($1,$2,$3,$4)`, [req.body.name, req.body.email, do_hash(req.body.password), req.body.phone]);
 		res.status(200).send("Successfully create an account");
 	} catch (error) {
 		res.status(500).send("Errors in the server");
@@ -26,7 +37,7 @@ async function registerAccount(req, res) {
 // login account function
 async function loginAccount(req, res) {
 	try {
-		const result = await db.query(`SELECT * FROM accounts where email = $1 and password = $2`, [req.body.email, req.body.password]);
+		const result = await db.query(`SELECT * FROM accounts where email = $1 and password = $2`, [req.body.email, do_hash(req.body.password)]);
 		if (result.rows.length == 1) res.status(200).send("Account login successful");
 		else res.status(401).send("Email or password is incorrect");
 	} catch (error) {
@@ -35,4 +46,6 @@ async function loginAccount(req, res) {
 
 }
 
-app.listen(port);
+app.listen(port, () => {
+	console.log(`Server started on port: ${port}`);
+});
