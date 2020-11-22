d0f67cc6c7fd05814e6f6e2c2f8a8bd59ac26cdc
TiDu <tidu.nguyen.2000@gmail.com>
2020-11-22 16:19:32 +0700

    test deploy

--- src/server/index.js
+++ src/server/index.js
@@ -1,38 +1,37 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 const session = require('express-session');
 const auth = require('./modules/authentication');
 const appointment = require('./modules/appointment');
 const port = process.env.PORT || 3001;
-//
-// app.use(express.json());
-// app.use(session({
-//     resave: true,
-//     saveUninitialized: true,
-//     secret: 'ShigeoTokuda',
-//     cookie: {maxAge: 31536000000}
-// }));
-app.use(express.static(path.join(__dirname, '../client/build')));
-// app.use(function (req, res, next) {
-//     res.set({
-//         'content-type': 'application/json',
-//         'access-control-allow-headers': 'origin, x-requested-with, content-type, accept',
-//         'access-control-allow-origin': '*',
-//         // 'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT'
-//     });
-//     next();
-// });
-//
-// // api
-// app.post("/user/register", auth.redirectHome, auth.registerAccount);
-// app.post("/user/login", auth.redirectHome, auth.loginAccount);
-// app.post("/user/logout", auth.logout);
-// app.post("/user/forget", auth.forgetPassword);
-// app.post("/user/reset/:userToken", auth.resetPassword);
-// app.post("/user/checkEmailExist", auth.checkEmailExist);
-//
-// app.post("/appointment/create", appointment.createAppointment);
+
+app.use(express.json());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'ShigeoTokuda',
+    cookie: {maxAge: 31536000000}
+}));
+app.use(express.static('../client/build'));
+app.use(function (req, res, next) {
+    res.set({
+        'content-type': 'application/json',
+        'access-control-allow-headers': 'origin, x-requested-with, content-type, accept',
+        'access-control-allow-origin': '*',
+        // 'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT'
+    });
+    next();
+});
+
+// api
+app.post("/user/register", auth.redirectHome, auth.registerAccount);
+app.post("/user/login", auth.redirectHome, auth.loginAccount);
+app.post("/user/logout", auth.logout);
+app.post("/user/forget", auth.forgetPassword);
+app.post("/user/reset/:userToken", auth.resetPassword);
+app.post("/user/checkEmailExist", auth.checkEmailExist);
+
+app.post("/appointment/create", appointment.createAppointment);
 
 
 app.listen(port, () => {
