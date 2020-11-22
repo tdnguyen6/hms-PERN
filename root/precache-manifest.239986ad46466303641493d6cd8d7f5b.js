5277a4fe12ff0a3b53acec621451347e6ca9b0bd
TiDu <tidu.nguyen.2000@gmail.com>
2020-11-22 15:23:22 +0700

    added root to test deployment

---
+++ root/precache-manifest.239986ad46466303641493d6cd8d7f5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70189b3b5d2fc4e9e17c4a6fa0e67d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01101bc2c4510e105056",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "af5d160b407d2c0f27cc",
+    "url": "/static/js/2.0cfee8e5.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.0cfee8e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01101bc2c4510e105056",
+    "url": "/static/js/main.14197660.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
