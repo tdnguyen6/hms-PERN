287e6b330fbf3640a78fa76b24724865fc8f8807
TiDu <tidu.nguyen.2000@gmail.com>
2020-11-24 21:45:55 +0700

    implemented 404 redirect for react

--- src/client/build/precache-manifest.ab58d0bcee83140e12e1b52aa714d4f2.js
+++ src/client/build/precache-manifest.ab58d0bcee83140e12e1b52aa714d4f2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d63ecf4d9e84ad694948266ac47a59f8",
+    "revision": "0fd83948c9edc641b731f4ccc4fa2a66",
     "url": "/index.html"
   },
   {
-    "revision": "ed4cd44bda6a4f275003",
+    "revision": "52417698863583cc0fad",
     "url": "/static/css/main.435d5fe5.chunk.css"
   },
   {
-    "revision": "2d1fa80d24b785e7aad0",
-    "url": "/static/js/2.89626c1b.chunk.js"
+    "revision": "5129fa09fa8e224c6d7b",
+    "url": "/static/js/2.cc13d36e.chunk.js"
   },
   {
     "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
-    "url": "/static/js/2.89626c1b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.cc13d36e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ed4cd44bda6a4f275003",
-    "url": "/static/js/main.a2a42a9c.chunk.js"
+    "revision": "52417698863583cc0fad",
+    "url": "/static/js/main.b6e10d2d.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- src/client/build/service-worker.js
+++ src/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56411d6109d5c99e579dedd09a415ce0.js"
+  "/precache-manifest.ab58d0bcee83140e12e1b52aa714d4f2.js"
 );
 
 self.addEventListener('message', (event) => {
