3c7124468fef6f8e0205706e536095a791669dc5
TiDu <tidu.nguyen.2000@gmail.com>
2020-11-24 10:54:46 +0700

    add 404.html

--- src/client/build/precache-manifest.d4a2f960a769e927e56831da5d403345.js
+++ src/client/build/precache-manifest.d4a2f960a769e927e56831da5d403345.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "70189b3b5d2fc4e9e17c4a6fa0e67d6c",
+    "revision": "d64b002aab65e15b24adcd1e7d1a47ec",
     "url": "/index.html"
   },
   {
-    "revision": "01101bc2c4510e105056",
+    "revision": "5c991d4d332816e760fd",
     "url": "/static/css/main.435d5fe5.chunk.css"
   },
   {
-    "revision": "af5d160b407d2c0f27cc",
-    "url": "/static/js/2.0cfee8e5.chunk.js"
+    "revision": "2e197b9613b95dad6d4a",
+    "url": "/static/js/2.2f6a964d.chunk.js"
   },
   {
     "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
-    "url": "/static/js/2.0cfee8e5.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.2f6a964d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "01101bc2c4510e105056",
-    "url": "/static/js/main.14197660.chunk.js"
+    "revision": "5c991d4d332816e760fd",
+    "url": "/static/js/main.2a39c7bb.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
