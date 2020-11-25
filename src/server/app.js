7e648a3cb875666510b230a31c38c57234933056
TiDu <tidu.nguyen.2000@gmail.com>
2020-11-25 23:00:55 +0700

    added app.js

---
+++ src/server/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session)
+const path = require('path');
+const auth = require('./modules/authentication');
+const appointment = require('./modules/appointment');
+
+app.use(express.json());
+app.use(session({
+    cookie: { maxAge: 86400000 },
+    saveUninitialized: true,
+    store: new MemoryStore({
+        checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    resave: true,
+    secret: 'Shigeo Tokuda'
+}))
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(function (req, res, next) {
+    res.set({
+        'content-type': 'application/json',
+        'access-control-allow-headers': 'origin, x-requested-with, content-type, accept',
+        'access-control-allow-origin': '*',
+        // 'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT'
+    });
+    next();
+});
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname + '/../client/public/404.html'));
+    res.status(404);
+    res.set({'content-type': 'text/html'});
+});
+
+// api
+app.post("/user/register", auth.redirectHome, auth.registerAccount);
+app.post("/user/login", auth.redirectHome, auth.loginAccount);
+app.post("/user/logout", auth.logout);
+app.post("/user/forget", auth.forgetPassword);
+app.post("/user/reset/:userToken", auth.resetPassword);
+app.post("/user/checkEmailExist", auth.checkEmailExist);
+
+app.post("/appointment/create", appointment.createAppointment);
+
+module.exports = app;
