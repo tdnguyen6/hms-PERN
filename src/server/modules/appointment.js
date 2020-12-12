d42ed2cc99070a23aa41696f303f405d560f9103
TienTai <Tientaiphan.joe@gmail.com>
2020-12-12 12:42:48 +0700

    Add pracID and patientID query all appointemnts for patient and practitioner

--- src/server/modules/appointment.js
+++ src/server/modules/appointment.js
@@ -14,7 +14,7 @@ exports.createAppointment = async function (req, res) {
 }
 
 exports.patientAppointments = async function (req, res) {
-    const queryStatement = "select ap.id, ap.room_id, m.name as medical_services, a.name as practitioner_name, to_char(ap.at, 'HH24:MM') as start, to_char(ap.at, 'DD/MM/YYYY') as date, ap.status, ap.log, ap.prescription, ap.next_appointment_period, (select m.name as next_appointment_service where ap.next_appointment_service = m.id), ap.last_appointment from appointments ap, rooms r, medicalservices m, practitioners p, accounts a where ap.room_id = r.id and r.medicalservice_id = m.id and ap.practitioner_id = p.id and p.id = a.practitioner_id and ap.patient_id = $1"
+    const queryStatement = "select ap.id, ap.room_id, m.name as medical_services, ap.practitioner_id, a.name as practitioner_name, to_char(ap.at, 'HH24:MM') as start, to_char(ap.at, 'DD/MM/YYYY') as date, ap.status, ap.log, ap.prescription, ap.next_appointment_period, (select m.name as next_appointment_service where ap.next_appointment_service = m.id), ap.last_appointment from appointments ap, rooms r, medicalservices m, practitioners p, accounts a where ap.room_id = r.id and r.medicalservice_id = m.id and ap.practitioner_id = p.id and p.id = a.practitioner_id and ap.patient_id = $1"
     const arr = [req.session.patientID]
     try {
         const result = await db.query(queryStatement, arr)
@@ -26,7 +26,7 @@ exports.patientAppointments = async function (req, res) {
 }
 
 exports.practitionerAppointments = async function (req, res) {
-    const queryStatement = "select ap.id, ap.room_id, m.name as medical_services, a.name as patient_name, date_part('year', age(dob)) as age, to_char(ap.at, 'HH24:MM') as start, to_char(ap.at, 'DD/MM/YYYY') as date, ap.status, ap.log, ap.prescription, ap.next_appointment_period, (select m.name as next_appointment_service where ap.next_appointment_service = m.id), ap.last_appointment from appointments ap, rooms r, medicalservices m, patients p, accounts a where ap.room_id = r.id and r.medicalservice_id = m.id and ap.patient_id = p.id and p.id = a.patient_id and ap.practitioner_id = $1"
+    const queryStatement = "select ap.id, ap.room_id, m.name as medical_services, ap.patient_id, a.name as patient_name, date_part('year', age(dob)) as age, to_char(ap.at, 'HH24:MM') as start, to_char(ap.at, 'DD/MM/YYYY') as date, ap.status, ap.log, ap.prescription, ap.next_appointment_period, (select m.name as next_appointment_service where ap.next_appointment_service = m.id), ap.last_appointment from appointments ap, rooms r, medicalservices m, patients p, accounts a where ap.room_id = r.id and r.medicalservice_id = m.id and ap.patient_id = p.id and p.id = a.patient_id and ap.practitioner_id = $1"
     const arr = [req.session.practitionerID]
     try {
         const result = await db.query(queryStatement, arr)
