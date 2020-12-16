d5a39691456ef6735d05dd7a6660bb1d087b7110
TienTai <Tientaiphan.joe@gmail.com>
2020-12-16 16:27:54 +0700

    Find disease by symptoms api now also return medical services associated with it

--- src/server/modules/disease.js
+++ src/server/modules/disease.js
@@ -6,47 +6,54 @@ const db = require('../db');
 }
 */
 exports.findDiseasesBySymptoms = async function (req, res) {
-    try {
-        console.log(req.body.symptoms)
-        let arr = req.body.symptoms
-        if (!arr || arr.length == 0) {
-            res.status(400).json(null)
-            return
-        }
+	const symptoms = req.body.symptoms
+	if (!symptoms || symptoms.length == 0) return res.status(400).json(null)
 
+    try {
         let queryString
-        if (arr.length === 1) {
-            queryString = "select ds1.disease_id as id, d.name, d.descriptions from diseases_symptoms ds1, diseases d where d.id = ds1.disease_id and ds1.symptom_id = " + arr[0]
-            console.log(queryString)
+        if (symptoms.length === 1) {
+												queryString = `select ds1.disease_id as id,
+																																		d.name         as disease,
+																																		m.name         as medical_service
+																											from diseases_symptoms ds1,
+																																diseases d,
+																																medicalservices m
+																											where d.id = ds1.disease_id and
+																																	d.suggested_checkup = m.id and
+																																	ds1.symptom_id = ` + symptoms[0]
         } else {
             queryString = "select ds1.disease_id from"
-            for (let i = 1; i <= arr.length; i++) {
-                if (i == arr.length) queryString += " diseases_symptoms ds" + i + "\n"
+            for (let i = 1; i <= symptoms.length; i++) {
+                if (i == symptoms.length) queryString += " diseases_symptoms ds" + i + "\n"
                 else queryString += " diseases_symptoms ds" + i + ","
             }
 
             // where ds1 = ds2, etc.
             queryString += "where ds2.disease_id = ds1.disease_id"
 
-            for (let i = 3; i <= arr.length; i++) {
-                if (i == arr.length) queryString += "\nand ds" + i + ".disease_id = ds1.disease_id"
+            for (let i = 3; i <= symptoms.length; i++) {
+                if (i == symptoms.length) queryString += "\nand ds" + i + ".disease_id = ds1.disease_id"
                 else queryString += "\nandds" + i + ".disease_id = ds1.disease_id,"
             }
 
             // and part
-            for (let i = 0; i < arr.length; i++) {
-                queryString += "\nand ds" + (i + 1) + ".symptom_id = " + arr[i]
+            for (let i = 0; i < symptoms.length; i++) {
+                queryString += "\nand ds" + (i + 1) + ".symptom_id = " + symptoms[i]
             }
             queryString = " (" + queryString + ")"
-            queryString = "select d.id, d.name from diseases d where d.id in" + queryString
-            console.log(queryString)
-        }
-
+												queryString = `select d.id, 
+																																		d.name as disease, 
+																																		m.name as medical_service
+																											from 		diseases d, 
+																																		medicalservices m
+																											where 	d.suggested_checkup = m.id and
+                  									d.id in` + queryString
+								}
         const result = await db.query(queryString)
-        res.status(200).json(result.rows)
+        return res.status(200).json(result.rows)
     } catch (err) {
         console.log(err)
-        res.status(500).json(null)
+        return res.status(500).json(null)
     }
 }
 
