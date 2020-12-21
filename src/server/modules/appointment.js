dff163993b25f62dfcf0231146af7d66f7654321
TienTai <Tientaiphan.joe@gmail.com>
2020-12-21 17:10:25 +0700

    Add recent appointment api

--- src/server/modules/appointment.js
+++ src/server/modules/appointment.js
@@ -243,7 +243,42 @@ exports.nAppointmentsByHour = async function (req, res) {
          console.log(err)
          return res.status(500).json({status: false})
      }
+}
+
+exports.recentAppointments = async function (req, res) {
+    const queryStatement = `
+    select to_char(at, 'DD/MM/YYYY') as date, 
+        count(id)
+    from appointments
+    where at > now() - '30 days'::interval and 
+    at < now()
+    group by date
+    order by date;
+    `
+
+ try {
+    const query_result = await db.query(queryStatement)
+    const _30RecentDates = recent30DatesArr()
+    const map = new Map()
+
+    _30RecentDates.forEach(element => map.set(element, 0))
+    query_result.rows.forEach(element => {
+        if (map.has(element.date)) map.set(element.date, element.count)
+    })
+
+    const result = []
+    map.forEach((value, key, map) => result.push({
+        date: key,
+        count: value
+    }))
+
+    return res.status(200).json(result)
+
+ } catch (err) {
+    console.log(err)
+    return res.status(500).json({status: false})
  }
+}
 
 exports.findRoom = async function (req, res) {
     if (!Number.isInteger(req.body.serviceID)) {
@@ -380,6 +415,24 @@ exports.deleteAppointment = async function (req, res) {
         return res.status(500).json({status: false})
     }
 }
+
+
+function recent30DatesArr() {
+    const date_Arr = []
+    for (let i=0; i<=30; i++) {
+        let date = new Date()
+        date.setDate(date.getDate() - i)
+        date_Arr.push(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`) 
+    }
+    return date_Arr
+}
+
+
+
+
+
+
+
 
 
 
